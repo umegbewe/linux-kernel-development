eef05469c13b7091b8515b6fc654dbcb93b0d6c2
Umegbewe Nwebedu <nwebedujunior55@gmail.com>
2021-08-12 22:06:19 +0100

    Added more check for packages

--- check.sh
+++ check.sh
@@ -4,6 +4,9 @@
 gcc --version | head -n1
 clang --version | head -n1
 make --version | head -n1
+git --version
+cscope --version 
+vim --version | head -n1
 ld -v
 flex --version
 bison --version | head -n1
@@ -12,3 +15,16 @@ depmod -V | head -n1
 e2fsck -V | head -n1
 fsck.jfs -V | head -n1
 reiserfsck -V
+mksquashfs -version | head -n1
+pccardctl -V
+quota -V | head -n1
+pppd --version | head -n1
+showmount --version
+ps --version
+udevd –version
+grub-install --version || grub --version
+mcelog --version
+iptables --version
+openssl version
+bc --version | head -n1
+sphinx-build --version

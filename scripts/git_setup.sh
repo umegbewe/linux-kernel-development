7900fe78eb0cd192586204e8acaabea13863a754
Orfeas Kazepis <orfeas.kazepis@gmail.com>
2022-03-29 02:35:16 +0300

    Help with compilation

--- scripts/git_setup.sh
+++ scripts/git_setup.sh
@@ -4,9 +4,9 @@ ORANGE="$(printf '\033[33m')" RESETBG="$(printf '\e[0m\n')"
 
 read -p "Enter full name: " name
 read -p "Enter email: " email
-read -p "Enter SMTP server: " smtp
+read -p "Enter SMTP server [gmail]: " smtp
+smtp=${smtp:-smtp.gmail.com}
 read -p "Enter your password ${ORANGE}[*security risk*]: ${RESETBG}" password
-echo "$name,$email,$smtp,$password"
 
 git config --global user.name "$name"
 git config --global user.email "$email"
